f751a71a6915422804fab3ea079e48a10d080ed1
Gleuton <gleuton.dutra@gmail.com>
2021-01-12 20:36:55 -0300

    entity interface

--- src/Entities/EntityInterface.php
+++ src/Entities/EntityInterface.php
@@ -6,5 +6,9 @@ namespace Gleuton\DataMapper\Entities;
 
 interface EntityInterface
 {
+    public function __construct(array $data = []);
 
+    public function setAll(array $data);
+
+    public function getAll(): array;
 }
